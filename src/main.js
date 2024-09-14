b74fb92d6b5c68c722742cd3f795502121e92738
koiibenvenutto <58677698+koiibenvenutto@users.noreply.github.com>
2024-09-13 20:06:25 -0700

    getting started, now I have custom code showing up in webflow

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
 import './styles/style.css'
+import feature from './styles/feature'
 
-console.log('Welcome to Vite + JS + Webflow!')
-
-createBadge()
-animateTitle()
+feature()
---
+++ src/styles/feature.js
@@ -0,0 +1,5 @@
+function feature() {
+  console.log('this is a feature')
+}
+
+export default feature
