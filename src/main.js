4abe7d1d5691d6dbe747f6c788d78617e3795d67
koiibenvenutto <58677698+koiibenvenutto@users.noreply.github.com>
2024-09-17 13:48:04 -0700

    removed document event listener that was causing error

--- src/main.js
+++ src/main.js
@@ -6,23 +6,23 @@ import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import SplitType from 'split-type'
 
-document.addEventListener('DOMContentLoaded', () => {
-  gsap.registerPlugin(ScrollTrigger)
-  // gsap code here!
+// document.addEventListener('DOMContentLoaded', () => {
+gsap.registerPlugin(ScrollTrigger)
+// gsap code here!
 
-  // SplitType: split text into words and characters
-  const text = new SplitType('#target', { types: 'words, chars' })
+// SplitType: split text into words and characters
+const text = new SplitType('#target', { types: 'words, chars' })
 
-  // Animate characters into view with a stagger effect
-  gsap.from(text.chars, {
-    opacity: 0.1,
-    stagger: 1,
-    duration: 3,
-    scrollTrigger: {
-      trigger: '.sticky-section',
-      start: 'top top+=50%',
-      end: 'bottom-=10% bottom',
-      scrub: true,
-    },
-  })
+// Animate characters into view with a stagger effect
+gsap.from(text.chars, {
+  opacity: 0.1,
+  stagger: 1,
+  duration: 3,
+  scrollTrigger: {
+    trigger: '.sticky-section',
+    start: 'top top+=50%',
+    end: 'bottom-=10% bottom',
+    scrub: true,
+  },
 })
+// })
